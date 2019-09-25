69faf9173f12d5ebdc7293272103937d69a2200b
LionBlue98 <LionBlue98@LAPTOP-1EO3F80H>
2019-09-25 14:08:42 +0100

    Final Project

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -5,6 +5,8 @@
  */
 package kata1;
 
+import java.util.Date;
+
 /**
  *
  * @author LionBlue98
@@ -15,7 +17,8 @@ public class Kata1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Person lion = new Person("Lion", new Date(98,3,14));
+        System.out.println(lion);
     }
     
 }
